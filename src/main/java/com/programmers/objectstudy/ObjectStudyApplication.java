24752e47ccdfadbec874d76891cd5553e6f4e4ea
킹받드라슈 <soccer_by@naver.com>
2022-08-25 01:02:49 +0900

    setting: springboot[gradle]

---
+++ src/main/java/com/programmers/objectstudy/ObjectStudyApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.objectstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmers/objectstudy/ObjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.objectstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
